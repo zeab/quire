358e162b5f062f9a32fac411fd029c9a9a2e6dda
kevin downs <pyrosrppltoo@gmail.com>
2018-12-22 01:18:58 -0800

    Update Routes.scala

--- src/main/scala/zeab/quireagent/webservice/Routes.scala
+++ src/main/scala/zeab/quireagent/webservice/Routes.scala
@@ -5,12 +5,10 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
-import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import zeab.quireagent.quireconductor.QuireConductorMessages.{CreateTopic, ProduceMsg}
 import zeab.quireagent.webservice.http.{PostProduceRequestBody, PostTopicRequestBody}
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
